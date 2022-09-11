7db42b9665008dbf1632b66c0da850d3b4274752
mithra-mkrk <mithra.muppi@gmail.com>
2022-09-11 19:29:09 +0530

    keys replace

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA2VUFKLBFJS3OCM42"
-  secret_key = "1A6vresH3bCv6O0SkysU0i8hNAD/B18CeZYMN00z"
+  access_key = "${file("../access_key.txt")}"
+  secret_key = "${file("../secret_key.txt")}"
 
 }
